fea0646cee00df3d056262cbde2c901d4f00a208
Ryotaro Shimada <k023c0012@g.neec.ac.jp>
2026-03-09 15:44:53 +0900

    日付機能の追加

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
+from datetime import date
 
 app = Flask(__name__)
 
@@ -16,12 +17,18 @@ def init_db():
         CREATE TABLE IF NOT EXISTS logs (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             content TEXT NOT NULL,
-            time TEXT NOT NULL
+            time TEXT NOT NULL,
+            study_date TEXT NOT NULL
         )
     """)
     conn.commit()
     conn.close()
 
+def add_date_column():
+    conn = get_db_connection()
+    conn.execute("ALTER TABLE logs ADD COLUMN study_date TEXT")
+    conn.commit()
+    conn.close()
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
@@ -32,16 +39,17 @@ def home():
         content = request.form['content']
 
         if study_time and content:
+            today = date.today().isoformat()
             conn.execute(
-                "INSERT INTO logs (content, time) VALUES (?, ?)",
-                (content, study_time)
+                "INSERT INTO logs (content, time,study_date) VALUES (?, ?, ?)",
+                (content, study_time,today)
             )
             conn.commit()
 
         conn.close()
         return redirect(url_for('home'))
 
-    logs = conn.execute("SELECT * FROM logs").fetchall()
+    logs = conn.execute("SELECT * FROM logs ORDER BY study_date DESC").fetchall()
     conn.close()
 
     return render_template('index.html', logs=logs)
